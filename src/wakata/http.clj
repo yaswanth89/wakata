0579eeea9c317abccace1022729a8184cfa72459
Yaswanth Yadlapalli <y.yaswanth89@gmail.com>
2015-08-27 22:13:25 +0530

    fixed routes bug
    Signed-off-by: Yaswanth Yadlapalli <y.yaswanth89@gmail.com>

--- src/wakata/http.clj
+++ src/wakata/http.clj
@@ -1,15 +1,16 @@
 (ns wakata.http
   (:require [ring.adapter.jetty :as jetty]
             [compojure.core :refer :all]
+            [compojure.route :as route]
             [wakata.controller :as controller]
             [ring.middleware.json :refer [wrap-json-response]]
             [ring.util.response :refer [response]]))
 
 (defroutes handler
-  (GET "/schedules" req (controller/schedules req)))
+  (GET "/schedules" [req] (response (controller/schedules req))))
 
 (def app
-  (wrap-json-response (response handler) ))
+  (wrap-json-response handler ))
 
 (defn start-server [port]
   (jetty/run-jetty app {:port port}))
